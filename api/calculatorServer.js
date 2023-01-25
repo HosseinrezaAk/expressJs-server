b7da40d928f58f94583ae4a3312a92e3ec7fb605
Hosseinreza <hrakbari77@gmail.com>
2023-01-26 02:14:16 +0330

    Add post

--- api/calculatorServer.js
+++ api/calculatorServer.js
@@ -6,7 +6,10 @@ app.get("/", function( req, res ){
     res.sendFile(__dirname + "/index.html");
 });
 
-
+// Whenever a post method called with action of "/" below method will be called
+app.post("/", function( req, res){
+    res.send("Hi");
+});
 app.listen(3000, function(){
     console.log("Server started on port http://localhost:3000");
 });
