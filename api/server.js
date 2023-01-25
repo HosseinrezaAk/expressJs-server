52b470b3aa1522369f4f696421dbe4dfb37487ab
Hosseinreza <hrakbari77@gmail.com>
2023-01-25 23:12:41 +0330

    Add url for about

--- api/server.js
+++ api/server.js
@@ -9,6 +9,11 @@ app.get("/", function( req, res ){   // req == request , res == response
 app.get("/contact", function( req , res ){
     res.send("Contact me at hrakbari77@gmail.com")
 });
+app.get("/about", function( req , res ){
+    res.send("I am Hossein a full-stack developer");
+})
+   
+
 app.listen(3000, function(){
     console.log("Server started on port http://localhost:3000");
 });
