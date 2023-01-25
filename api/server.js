88d845d960f9a6a9f036b3705ceb13b9d360f4a4
Hosseinreza <hrakbari77@gmail.com>
2023-01-25 23:10:53 +0330

    Add contact address

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function( req, res){   // req == request , res == response
+app.get("/", function( req, res ){   // req == request , res == response
 
-    res.send("hello");
+    res.send("<h1>Hello World</h1>");
 
-})
+});
+app.get("/contact", function( req , res ){
+    res.send("Contact me at hrakbari77@gmail.com")
+});
 app.listen(3000, function(){
     console.log("Server started on port http://localhost:3000");
 });
