df24112a513b3e108ef5d07b43e0d404ee769623
Hosseinreza <hrakbari77@gmail.com>
2023-01-25 23:02:54 +0330

    Add response to root /

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function( request, response){
+
+    response.send("hello");
+
+})
 app.listen(3000, function(){
-    console.log("Server started on port 3000");
+    console.log("Server started on port http://localhost:3000");
 });
