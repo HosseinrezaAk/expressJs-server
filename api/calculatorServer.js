20f22f162909607cbcbef4f81bd9e8453cdb9b8d
Hosseinreza <hrakbari77@gmail.com>
2023-01-27 02:47:14 +0330

    Add post and get for bmi

--- api/calculatorServer.js
+++ api/calculatorServer.js
@@ -17,6 +17,17 @@ app.post("/", function( req, res){
     var result = num1 + num2;
     res.send("The result is :" + result);
 });
+
+app.get("/bmicalculator", function( req , res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmicalculator", function( req , res){
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+    var bmi = height/Math.pow( weight , 2 );
+    res.send("The BMI is : " + bmi);
+
+});
 app.listen(3000, function(){
     console.log("Server started on port http://localhost:3000");
 });
